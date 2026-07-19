7b9d55c8688934668723a2e70b549ee590e1a8be
Shrinandan kumar <shrinandank326@gmail.com>
2026-07-19 14:56:28 +0530

    react Practice
    creating project

--- Week5/vite.config.js
+++ Week5/vite.config.js
@@ -1,11 +1,21 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    open: true
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/comment': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      }
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
   }
 })
